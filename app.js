84fada37ce4178ec45410236490429d5fdd1de44
Veerawat Sirivong <meeyokking@gmail.com>
2023-11-05 01:59:01 +0700

    co

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const icon = document.querySelector("i")
 const text = document.querySelector("h2")
 
 icon.addEventListener("click",function(){
-    this.classList.toggle("bi bi-moon-stars")
+    this.classList.toggle("bi-moon-stars")
 })
